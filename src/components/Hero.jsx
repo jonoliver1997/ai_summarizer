4f64d78e785cf5075594985bdb28f87b4ab7ab3b
Jonathan Oliver <jonoliver1997@gmail.com>
2024-08-03 10:27:05 -0700

    add ai summarizer api

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,26 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
         <button
           type="button"
-          onClick=""
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() =>
+            window.open("https://github.com/jonoliver1997/ai_summarizer.git")
+          }
+          className="black_btn"
         >
-          {" "}
           Github{" "}
         </button>
       </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAi GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries.
+      </h2>
     </header>
   );
 };
